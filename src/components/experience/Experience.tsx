f9937060e233c8a52ea30eef19a643059333054f
Hamza Mehmood <hamza.mahmood@zho.gov.ae>
2023-04-17 00:27:23 +0400

    skills added

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
 import './styles.css'
+import { BsPatchCheckFill } from "react-icons/bs"
+import { useUserDataSet } from '../../hooks/useUserDataSet'
 const Experience = () => {
+    const data = useUserDataSet()
+
     return (
-        <section id="experience">Experience</section>
+        <section id="experience">
+
+            <h5>
+                What skills I have
+            </h5>
+            <h2>
+                My Skills
+            </h2>
+            <div className="container experience__container">
+                <div className="experience__card">
+                    <div className="expereince__content">
+                        {
+                            data.skills.map(item => {
+                                return (< article className="experience__details" >
+                                    <BsPatchCheckFill />
+                                    <small className='text-light'>
+                                        {item}
+                                    </small>
+                                </article>)
+                            })
+                        }
+
+                    </div>
+                </div>
+            </div>
+        </section >
     )
 }
 
